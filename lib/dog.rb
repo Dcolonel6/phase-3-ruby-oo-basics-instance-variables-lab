4686e6a7d05c23b831996164287d41dc84bbee9f
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-31 11:00:15 +0300

    finished assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    #this is a setter mthod
+    def name=(dog_name)
+        #this is now an instance variable
+        @this_dogs_name = dog_name
+    end
+
+    #this is a getter metthod
+    def name
+        @this_dogs_name
+    end
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name
